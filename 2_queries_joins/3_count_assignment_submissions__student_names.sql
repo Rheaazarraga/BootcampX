0aca45550e5e4d3742866c5536b284744071694b
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-30 02:42:58 +0000

    Add comment for reference

--- 2_queries_joins/3_count_assignment_submissions__student_names.sql
+++ 2_queries_joins/3_count_assignment_submissions__student_names.sql
@@ -3,3 +3,5 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.end_date IS NULL
 GROUP BY students.name;
+
+-- GROUP BY allows us to combine the results based on a column so an aggregate can be applied to each group.
