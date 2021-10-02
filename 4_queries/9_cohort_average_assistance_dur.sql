eea4642daf4cb2377dca9f1bb7d365f0d37211b1
Rheaazarraga <rheaazarraga@gmail.com>
2021-10-02 22:13:45 +0000

    Add query with correct logic for avg total duration per cohort

--- 4_queries/9_cohort_average_assistance_dur.sql
+++ 4_queries/9_cohort_average_assistance_dur.sql
@@ -3,12 +3,13 @@
 -- Use the previous query as a sub query to determine the duration per cohort.
 
 -- Return a single row average_total_duration
-SELECT avg(total_duration)
+
+SELECT avg (total_duration) as average_total_duration
 FROM (
-  SELECT sum (completed_at-started_at) as total_duration
+  SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
   FROM assistance_requests
-  JOIN students ON students.id = assistance_requests.student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  GROUP BY cohorts.name 
+  JOIN students ON students.id = student_id
+  JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
   ORDER BY total_duration
-) as average_total_duration;
+) as total_durations;
